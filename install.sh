3d95a9451acd04e0e1c9c7495d3bc70ed513205e
miro <jarbasai@mailfence.com>
2025-01-22 05:13:56 +0000

    wip

--- install.sh
+++ install.sh
@@ -4,10 +4,11 @@
 # This script installs the necessary services, rules, and scripts for configuring sound on raspOVOS.
 
 # Define paths for the services and scripts
-AUTOCONFIGURE_SERVICE_PATH="./autoconfigure_soundcard.service"
-COMBINED_SINKS_SERVICE_PATH="./combine_sinks.service"
+AUTOCONFIGURE_SERVICE_PATH="./autoconfigure_soundcard.service"  # runs on boot
+COMBINED_SINKS_SERVICE_PATH="./combine_sinks.service"  # for udev rule usage
 SOUNDCARD_AUTOCONFIGURE_SCRIPT_PATH="./soundcard-autoconfigure"
 USB_AUTOVOLUME_SCRIPT_PATH="./usb-autovolume"
+OVOS_AUDIO_DIAGNOSTICS_SCRIPT_PATH="./ovos-audio-diagnostics"
 OVOS_AUDIO_SETUP_SCRIPT_PATH="./ovos-audio-setup"
 UPDATE_AUDIO_SINKS_SCRIPT_PATH="./combine-sinks"
 
@@ -15,7 +16,7 @@ UPDATE_AUDIO_SINKS_SCRIPT_PATH="./combine-sinks"
 SYSTEMD_SERVICE_DIR="/etc/systemd/system"
 
 # Install systemd services
-echo "Installing systemd services..."
+echo "Installing systemd service..."
 
 sudo cp "$AUTOCONFIGURE_SERVICE_PATH" "$SYSTEMD_SERVICE_DIR"
 sudo cp "$COMBINED_SINKS_SERVICE_PATH" "$SYSTEMD_SERVICE_DIR"
@@ -27,20 +28,22 @@ sudo systemctl daemon-reload
 # Install other scripts to /usr/local/bin and /usr/libexec
 echo "Installing additional scripts..."
 sudo cp "$OVOS_AUDIO_SETUP_SCRIPT_PATH" "/usr/local/bin/ovos-audio-setup"
+sudo cp "$OVOS_AUDIO_DIAGNOSTICS_SCRIPT_PATH" "/usr/local/bin/ovos-audio-diagnostics"
 sudo cp "$UPDATE_AUDIO_SINKS_SCRIPT_PATH" "/usr/libexec/combine-sinks"
-sudo cp "$SOUNDCARD_AUTOCONFIGURE_SCRIPT_PATH" "/usr/libexec/soundcard-autoconfigure"
+sudo cp "$SOUNDCARD_AUTOCONFIGURE_SCRIPT_PATH" "/usr/libexec/soundcard_autoconfigure"
 sudo cp "$USB_AUTOVOLUME_SCRIPT_PATH" "/usr/libexec/usb-autovolume"
 
 # Ensure scripts are executable
 echo "Setting executable permissions for the scripts..."
-
 sudo chmod +x "/usr/local/bin/ovos-audio-setup"
 sudo chmod +x "/usr/libexec/combine-sinks"
-sudo chmod +x "/usr/libexec/soundcard-autoconfigure"
+sudo chmod +x "/usr/local/bin/ovos-audio-diagnostics"
+sudo chmod +x "/usr/libexec/soundcard_autoconfigure"
 sudo chmod +x "/usr/libexec/usb-autovolume"
 
 # Install dependencies
-apt-get install -y --no-install-recommends pulseaudio-utils
+# TODO - detect sound server and prompt user about migrating to pipewire if needed
+apt-get install -y --no-install-recommends wireplumber
 
 # Check if i2csound.service exists
 if [ ! -f /etc/systemd/system/i2csound.service ]; then
